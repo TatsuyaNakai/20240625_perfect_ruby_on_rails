8a15fb8421241918830c1efda08fa20a55599c1b
tatsuya.nakai <15g079nt@gmail.com>
2024-06-25 18:03:34 +0900

    minitest

---
+++ test/models/task_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class TaskTest < ActiveSupport::TestCase
+  # test メソッドで実行します
+  # test "the truth" do
+  #   assert true
+  # assertにて、結果を検証します。(assert_XXX系があります)
+  # https://qiita.com/TomoProg/items/fe99e7dde5e901d0673f
+  # end
+end
