c3eb1ff7b08b4ffdff4f909df8ba2a9af0961a0f
tatsuya.nakai <15g079nt@gmail.com>
2024-06-28 17:27:38 +0900

    authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,21 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in?
+  helper_method %i[logged_in? current_user]
+
+  before_action :authenticate
 
   private
 
   def logged_in?
     !!session[:user_id]
+  end
+
+  def authenticate
+    return if logged_in?
+    
+    redirect_to root_path, alert: 'ログインしてください'
+  end
+
+  def current_user
+    User.find(session[:user_id])
   end
 end
