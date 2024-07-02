e246c11816162176e5c57660af8ff526f8e8695c
tatsuya.nakai <15g079nt@gmail.com>
2024-07-02 11:11:32 +0900

    feat: model test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "created_by? owner_idとuser#idが同じ場合" do
+    event = FactoryBot.create(:event)
+    user = User.new
+    user.stub(:id, event.owner_id) do
+      assert_equal(true, event.created_by?(user))
+    end
+  end
+
+  test "created_by? owner_idとuser#idが異なる場合" do
+    event = FactoryBot.create(:event)
+    user = FactoryBot.create(:user)
+    assert_equal(false, event.created_by?(user))
+  end
+
+  test "created_by? 引数がnilの場合" do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.created_by?(nil))
+  end
 end
