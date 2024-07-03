fa311a474eb27f01b0d28b751b50b68c229793cf
tatsuya.nakai <15g079nt@gmail.com>
2024-07-03 18:02:31 +0900

    feat: SimpleCov

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ group :development do
   # Can be configured to work on production as well see: https://github.com/MiniProfiler/rack-mini-profiler/blob/master/README.md
   gem 'rack-mini-profiler', '~> 2.0'
   gem 'listen', '~> 3.3'
+
+  gem 'simplecov', require: false
 end
 
 group :test do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
+# どのファイルを読み込むよりも先に実施してください。
+require "simplecov"
+SimpleCov.start 'rails'
+
 require_relative "../config/environment"
 require_relative './support/sign_in_helper.rb'
 require "rails/test_help"
@@ -6,7 +10,7 @@ require "minitest/mock"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors) # カバレッジ測定(SimpleCov実行)時には、並列テストの設定を切るようにしてください。
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
